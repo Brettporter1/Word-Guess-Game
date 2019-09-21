a2aaa791e594e453fb3bc2170c2b13f49cfb4761
Brett Porter <gkdesign06@gmail.com>
2019-09-21 09:02:17 -0500

    working through getting letter to populate spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+
 var wordChoices = ["carabiner", "rope", "belay", "harness", "whipper", "granite", "himalaya", "jumar", "summit", "crimp"];
 var selectedWord = "";
 var wordLetters = [];
@@ -7,12 +9,16 @@ var wrongGuesses = [];
 var wins = 0;
 var losses = 0;
 var numOfGuesses = 10;
+var letterInWord = false;
 
 
-
-
-
+// load the game function
 function runGame() {
+    // set start values
+    wins = 0;
+    losses = 0
+    numOfGuesses = 10;
+    wrongGuesses = [];
     // select random word
     var selectedWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
@@ -23,22 +29,51 @@ function runGame() {
     for (var i = 0; i < wordLetters.length;i++){
         blankSpaces.push("_");
     }
-    // write spaces to the DOM
-    blankSpaces = blankSpaces.join(" ");
-    $(".theWord").text(blankSpaces);
-    $('.winCount').text('wins ' + wins);
-    $('.lossCount').text('losses ' + losses);
-    $('.guessesLeft').text('guesses ' + numOfGuesses);
-
+    console.log(wordLetters);
     console.log(blankSpaces);
-    console.log(selectedWord);
-
+    // write spaces to the DOM
+    document.getElementById("theWord").innerHTML = blankSpaces.join(" ");
+    document.getElementById('winCount').innerHTML = wins;
+    document.getElementById('lossCount').innerHTML = losses;
+    document.getElementById('guessesLeft').innerHTML = numOfGuesses;
+    document.getElementById('wrongLetters').innerHTML = wrongGuesses.join(" ");
 }
+// play game functions
 
+// find the guessed letter in word
+function findInWord(letter){
+    var letterInWord = false;
+    for (var i = 0; i < wordLetters.length;i++){
+        if(wordLetters[i] === letter){
+            letterInWord = true;
+        }
+     }
+     // if letter found replace the spaces with guessed letter
+     if(letterInWord){
+         for(var i = 0; i < wordLetters.length;i++){
+            if(wordLetters[i] === letter){
+             blankSpaces[i] = letter;
+            }
+         }
+     }
+     // if not found 
+     else{
+         wrongGuesses.push(letter);
+         numOfGuesses--
+     }
+    }
+
+
+
+
+
+
+
+// call functions
 runGame();
 
 document.onkeyup = function(event){
-    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();        
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase(); 
     console.log(letterGuessed);
-
+    findInWord(letterGuessed);
 }
