7ab8415fe4126d0311685dacd920daec9b42d90b
Brett Porter <gkdesign06@gmail.com>
2019-09-21 22:28:34 -0500

    Fixed letters populating on DOM, need to fix multiple wrong letter guessing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,10 +15,9 @@ var letterInWord = false;
 // load the game function
 function runGame() {
     // set start values
-    wins = 0;
-    losses = 0
     numOfGuesses = 10;
     wrongGuesses = [];
+    blankSpaces = [];
     // select random word
     var selectedWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
@@ -26,45 +25,66 @@ function runGame() {
     wordLetters = selectedWord.split("");
 
     // create spaces for each letter.
-    for (var i = 0; i < wordLetters.length;i++){
+    for (var i = 0; i < wordLetters.length; i++) {
         blankSpaces.push("_");
     }
     console.log(wordLetters);
     console.log(blankSpaces);
     // write spaces to the DOM
     document.getElementById("theWord").innerHTML = blankSpaces.join(" ");
-    document.getElementById('winCount').innerHTML = wins;
-    document.getElementById('lossCount').innerHTML = losses;
-    document.getElementById('guessesLeft').innerHTML = numOfGuesses;
-    document.getElementById('wrongLetters').innerHTML = wrongGuesses.join(" ");
+    document.getElementById("winCount").innerHTML = wins;
+    document.getElementById("lossCount").innerHTML = losses;
+    document.getElementById("guessesLeft").innerHTML = numOfGuesses;
 }
 // play game functions
 
 // find the guessed letter in word
-function findInWord(letter){
+function findInWord(letter) {
     var letterInWord = false;
-    for (var i = 0; i < wordLetters.length;i++){
-        if(wordLetters[i] === letter){
+    for (var i = 0; i < wordLetters.length; i++) {
+        if (wordLetters[i] === letter) {
             letterInWord = true;
+            console.log(letter);
         }
-     }
-     // if letter found replace the spaces with guessed letter
-     if(letterInWord){
-         for(var i = 0; i < wordLetters.length;i++){
-            if(wordLetters[i] === letter){
-             blankSpaces[i] = letter;
+    }
+    // if letter found replace the spaces with guessed letter
+    if (letterInWord) {
+        for (var i = 0; i < wordLetters.length; i++) {
+            if (wordLetters[i] === letter) {
+                console.log([i]);
+                blankSpaces[i] = letter;
+                document.getElementById("theWord").innerHTML = blankSpaces.join(' ');
             }
-         }
-     }
-     // if not found 
-     else{
-         wrongGuesses.push(letter);
-         numOfGuesses--
-     }
+        }
+    }
+    // if not found 
+    else {
+        wrongGuesses.push(letter);
+        numOfGuesses--;
+        document.getElementById("guessesLeft").innerHTML = numOfGuesses;
+        console.log(wrongGuesses);
     }
 
+    
+    
+    
+    document.getElementById("wrongLetters").innerHTML = wrongGuesses.join(" ").toUpperCase();
+}
 
-
+function winOrLose(){
+    if(wordLetters.toString() === blankSpaces.toString()){
+        wins++;
+        alert('Success!')
+        document.getElementById("winCount").innerHTML = wins;
+        runGame();
+    }
+    else if (numOfGuesses === 0){
+        losses++;
+        alert('Mistakes are Costly!');
+        document.getElementById("lossCount").innerHTML = losses;
+        runGame();
+    }
+}
 
 
 
@@ -72,8 +92,8 @@ function findInWord(letter){
 // call functions
 runGame();
 
-document.onkeyup = function(event){
-    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase(); 
-    console.log(letterGuessed);
+document.onkeyup = function (event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
     findInWord(letterGuessed);
+    winOrLose();
 }
